eebbed3179bfdb6712f7ff2bfa6ac2ce03939c9b
Fernando Robles <fernando.robles@omnitracs.com>
2020-09-02 17:36:08 -0500

    Add In-app Navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,25 +12,12 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
-  selectedHero: Hero;
-
   constructor(
-    private heroService: HeroService, 
-    private messageService: MessageService
+    private heroService: HeroService
   ) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`Heroes component: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
